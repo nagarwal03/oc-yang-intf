71a5ebd289c60cf28dd472e824bb6bef0d25a2fe
Satoru-Shinohara <satoru.shinohara@dell.com>
2024-05-13 19:27:29 -0400

    address azure-pipelines error

--- translib/transformer/interfaces_openconfig_test.go
+++ translib/transformer/interfaces_openconfig_test.go
@@ -149,7 +149,7 @@ func Test_openconfig_ethernet(t *testing.T) {
 
 	cleanuptbl := map[string]interface{}{"PORT_TABLE": map[string]interface{}{"Ethernet0": ""}}
 	unloadDB(db.ApplDB, cleanuptbl)
-	pre_req_map := map[string]interface{}{"PORT_TABLE": map[string]interface{}{"Ethernet0": map[string]interface{}{"admin_status": "up", "autoneg": "on", "mtu": "9100", "speed": "40000"}}}
+	pre_req_map := map[string]interface{}{"PORT_TABLE": map[string]interface{}{"Ethernet0": map[string]interface{}{"admin_status": "up", "autoneg": "on", "mtu": "9100", "speed": "40000", "counters": map[string]interface{}{"in-broadcast-pkts": "0", "in-discards": "0", "in-errors": "0", "in-multicast-pkts": "0", "in-octets": "0", "in-pkts": "0", "in-unicast-pkts": "0", "out-broadcast-pkts": "0", "out-discards": "0", "out-errors": "0", "out-multicast-pkts": "0", "out-octets": "0", "out-pkts": "0", "out-unicast-pkts": "0"}}}}
 	loadDB(db.ApplDB, pre_req_map)
 
 	t.Log("\n\n--- Verify PATCH ethernet ---")
